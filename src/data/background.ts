5c5cf0f24cfb866da969e602ae3efa5ceb1ba44a
Jan Podmolík <jan.podmolik@gmail.com>
2025-12-02 11:04:41 +0100

    Fix parallax movements.

--- src/data/background.ts
+++ src/data/background.ts
@@ -16,7 +16,7 @@ export const AVAILABLE_BACKGROUNDS: BackgroundConfig[] = [
     name: 'FOREST GREEN',
     folder: 'forest_green',
     // forest_green has layers 1-6 (6 parallax layers, last 1 is foreground)
-    scrollFactors: [0.75, 0.8, 0.85, 0.9, 0.95, 1.0],
+    scrollFactors: [0.75, 0.8, 0.85, 0.9, 1.0, 1.0],
     foregroundLayers: 1,
   },
   {
@@ -29,33 +29,33 @@ export const AVAILABLE_BACKGROUNDS: BackgroundConfig[] = [
     name: 'FOREST BIRCH',
     folder: 'forest_birch',
     // forest_birch has layers 1-5 (5 parallax layers)
-    scrollFactors: [0.75, 0.8, 0.85, 0.9, 0.95],
+    scrollFactors: [0.75, 0.8, 0.85, 0.9, 1.0],
   },
   {
     name: 'FOREST FANTASY',
     folder: 'forest_fantasy',
     // forest_fantasy has layers 1-7 (7 parallax layers, last 2 are foreground)
-    scrollFactors: [0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.0],
+    scrollFactors: [0.75, 0.8, 0.85, 0.9, 1.0, 1.0, 1.0],
     foregroundLayers: 2,
   },
   {
     name: 'FOREST GOLD',
     folder: 'forest_gold',
     // forest_gold has layers 1-6 (6 parallax layers, last 2 are foreground)
-    scrollFactors: [0.75, 0.8, 0.85, 0.9, 1.0, 1.0],
+    scrollFactors: [0.75, 0.8, 0.85, 1.0, 1.0, 1.0],
     foregroundLayers: 2,
   },
   {
     name: 'FOREST JUNGLE',
     folder: 'forest_jungle',
     // forest_jungle has layers 1-4 (4 parallax layers)
-    scrollFactors: [0.75, 0.8, 0.85, 0.9],
+    scrollFactors: [0.75, 0.8, 0.85, 1.0],
   },
   {
     name: 'FOREST SUMMER',
     folder: 'forest_summer',
     // forest_summer has layers 1-5 (5 parallax layers, last 1 is foreground)
-    scrollFactors: [0.75, 0.8, 0.85, 0.9, 1.0],
+    scrollFactors: [0.75, 0.8, 0.85, 1.0, 1.0],
     foregroundLayers: 1,
   },
 ];
