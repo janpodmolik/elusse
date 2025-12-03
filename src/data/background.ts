19af2bab39a3a11de36f712c0f05dbd03df16dcf
Jan Podmolík <jan.podmolik@gmail.com>
2025-12-03 12:55:11 +0100

    New cave background.

--- src/data/background.ts
+++ src/data/background.ts
@@ -50,7 +50,8 @@ export const AVAILABLE_BACKGROUNDS: BackgroundConfig[] = [
     name: 'FOREST JUNGLE',
     folder: 'forest_jungle',
     // forest_jungle has layers 1-4 (4 parallax layers)
-    scrollFactors: [0.75, 0.8, 0.85, 1.0],
+    scrollFactors: [0.75, 0.8, 0.95, 1.0],
+    foregroundLayers: 1,
   },
   {
     name: 'FOREST SUMMER',
@@ -58,6 +59,13 @@ export const AVAILABLE_BACKGROUNDS: BackgroundConfig[] = [
     // forest_summer has layers 1-5 (5 parallax layers, last 1 is foreground)
     scrollFactors: [0.75, 0.8, 0.85, 1.0, 1.0],
     foregroundLayers: 1,
+  },
+  {
+    name: 'CAVE DARK',
+    folder: 'cave_dark',
+    // cave_dark has layers 1-7 (7 parallax layers, last 2 are foreground)
+    scrollFactors: [0.7, 0.75, 0.8, 0.85, 0.9, 1.0, 1.0],
+    foregroundLayers: 2,
   },
 ];
 
